53a88354756702ddf66fb03b605259bbee0b292a
dukewellington61 <dukewellington61@googlemail.com>
2019-01-21 20:37:24 +0100

    4.1 created seed.db to migrate local db to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!([
+  {name: "generic looking bike 1", description: "a rather generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
+  {name: "generic looking bike 2", description: "another rather generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
+  {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
+  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"}
+])
