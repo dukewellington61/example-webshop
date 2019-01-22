3612518055576b4874a89d492bc79cc255800f47
dukewellington61 <dukewellington61@googlemail.com>
2019-01-23 00:09:12 +0100

    4.2 db add column colour and price some more css to product pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@ Product.create!([
   {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
   {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"}
 ])
+
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
