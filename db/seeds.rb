69275f043e7574a0bf5cdae906753cb4d1982228
dukewellington61 <dukewellington61@googlemail.com>
2019-01-23 00:16:21 +0100

    4.2 push seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 Product.create!([
-  {name: "generic looking bike 1", description: "a rather generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
-  {name: "generic looking bike 2", description: "another rather generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
-  {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"},
-  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "http://localhost:3000/assets/race_bike-dba5e269cf248f71fa3a156c148ab193ce1dc4fd031ea91c00a8cd129ab3681d.png"}
+  {name: "generic looking bike 1", description: "a rather generic looking bike", image_url: "race_bike.png", colour: "black", price: "450.99"},
+  {name: "generic looking bike 2", description: "another rather generic looking bike", image_url: "race_bike.png", colour: "black white", price: "499.99"},
+  {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "race_bike.png", colour: "black white grey", price: "549.99"},
+  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "race_bike.png", colour: "black white", price: "949.99"}
 ])
-
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
