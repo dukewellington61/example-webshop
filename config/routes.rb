75ba746f1d6218a5a26a80faaef261c2e6f6d983
dukewellington61 <dukewellington61@googlemail.com>
2019-01-29 19:56:45 +0100

    5.1 reroute root page, redirect_to @product, navbar links dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'  
+  root 'simple_pages#about'  
   resources :orders, only: [:index, :show, :create, :destroy]
 end
