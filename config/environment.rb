1e6a3104425027cb2073603b97ba5105c7d4b7d6
dukewellington61 <dukewellington61@googlemail.com>
2019-02-01 10:26:27 +0100

    5.3 email sendgrit user name password added

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['dukewellington'],
+  password: ENV['qyM784fpQm5A6QtJp'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
