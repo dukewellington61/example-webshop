b455103b5aedcf10458fcd639fe87de6f578f743
dukewellington61 <dukewellington61@googlemail.com>
2019-02-02 16:44:38 +0100

    5.4 user database +  Device authentication branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  resources :users
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
-  get 'simple_pages/thank_you'
+  get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#about'  
+  root 'simple_pages#landing_page'  
   resources :orders, only: [:index, :show, :create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
+User.create!([
+  {first_name: nil, last_name: nil, email: "dukewellington@gmx.de", encrypted_password: "$2a$11$INHxnwlVcvnuGLOcWB4qRuOyf.cx8qmpn7Jprv35h4Uu5mRP8BRRS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+])
 Order.create!([
-  {user_id: 1, product_id: 24, total: 10.0},
-  {user_id: 1, product_id: 25, total: 5.0}
+  {user_id: 1, product_id: 1, total: 10.0},
+  {user_id: 1, product_id: 3, total: 5.0}
 ])
 Product.create!([
   {name: "generic looking bike 1", description: "a rather generic looking bike", image_url: "adventure-beach-bicycle-462036.jpg", colour: "black", price: "450.99"},
   {name: "generic looking bike 2", description: "another rather generic looking bike", image_url: "mikkel-bech-748940-unsplash.jpg", colour: "black white", price: "499.99"},
   {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "race_bike.png", colour: "black white grey", price: "549.99"},
-  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "race_bike.png", colour: "black white", price: "949.99"}
+  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "race_bike.png", colour: "black white", price: "949.99"},
+  {name: "Vintage Bike", description: "old stuff", image_url: "bicycle-1296407_1280.png", colour: "black", price: "1000.0"},
+  {name: "", description: "", image_url: "", colour: "", price: nil},
+  {name: "", description: "", image_url: "", colour: "", price: nil},
+  {name: "3453453", description: "", image_url: "", colour: "", price: nil},
+  {name: nil, description: "", image_url: "", colour: "", price: nil},
+  {name: "!§)$($)", description: "", image_url: "", colour: "", price: nil},
+  {name: "I like chocolate", description: "sweet", image_url: "/home/euklid/carrerFoundry Web Dev Course/nameofapp/app/assets/images/race_bike.png", colour: "brown", price: "1.99"},
+  {name: "Vintage Bike2", description: "weegweg", image_url: "/assets/images/race_bike.png", colour: "dfrhdh", price: "4.0"}
 ])
