7d6f703ce4975ab751fef40a9231142b8b8b5120
dukewellington61 <dukewellington61@googlemail.com>
2019-02-02 17:40:48 +0100

    5.4 trouble shooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'duke-wellington-example-app.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: "duke-wellington-example-app.herokuapp.com" }
 
 end
 
