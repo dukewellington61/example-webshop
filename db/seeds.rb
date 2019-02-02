cafd5fd5ed63b5e62bc7420a8613d0a7cf284ee6
dukewellington61 <dukewellington61@googlemail.com>
2019-02-02 20:16:22 +0100

    5.4 seed products only

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,6 @@
-User.create!([
-  {first_name: nil, last_name: nil, email: "dukewellington@gmx.de", encrypted_password: "$2a$11$INHxnwlVcvnuGLOcWB4qRuOyf.cx8qmpn7Jprv35h4Uu5mRP8BRRS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
-])
-Order.create!([
-  {user_id: 1, product_id: 1, total: 10.0},
-  {user_id: 1, product_id: 3, total: 5.0}
-])
 Product.create!([
-  {name: "generic looking bike 1", description: "a rather generic looking bike", image_url: "adventure-beach-bicycle-462036.jpg", colour: "black", price: "450.99"},
-  {name: "generic looking bike 2", description: "another rather generic looking bike", image_url: "mikkel-bech-748940-unsplash.jpg", colour: "black white", price: "499.99"},
-  {name: "generic looking bike 3", description: "yet another generic looking bike", image_url: "race_bike.png", colour: "black white grey", price: "549.99"},
-  {name: "generic looking bike 4", description: "that's right! another generic looking bike.", image_url: "race_bike.png", colour: "black white", price: "949.99"},
-  {name: "Vintage Bike", description: "old stuff", image_url: "bicycle-1296407_1280.png", colour: "black", price: "1000.0"},
-  {name: "", description: "", image_url: "", colour: "", price: nil},
-  {name: "", description: "", image_url: "", colour: "", price: nil},
-  {name: "3453453", description: "", image_url: "", colour: "", price: nil},
-  {name: nil, description: "", image_url: "", colour: "", price: nil},
-  {name: "!§)$($)", description: "", image_url: "", colour: "", price: nil},
-  {name: "I like chocolate", description: "sweet", image_url: "/home/euklid/carrerFoundry Web Dev Course/nameofapp/app/assets/images/race_bike.png", colour: "brown", price: "1.99"},
-  {name: "Vintage Bike2", description: "weegweg", image_url: "/assets/images/race_bike.png", colour: "dfrhdh", price: "4.0"}
+  {name: "Mountain Challenger", description: "Sleek yet Robust - Perfect for high speed downhill rides of the breakneck variety!", image_url: "adventure-beach-bicycle-462036.jpg", colour: "black", price: "949.95"},
+  {name: "Elegant Racer", description: "Rapid and Stylish - Perfect for covering long distances at high velocity!", image_url: "mikkel-bech-748940-unsplash.jpg", colour: "black white", price: "1499.99"},
+  {name: "Single Speed Beauty", description: "Quick and Graceful - Perfect for riding around town in style. Discover the Hipster in you!", image_url: "robert-bye-200735-unsplash.jpg", colour: "white", price: "949.99"},
+  {name: "Old Time Standard", description: "Unique and Daring - Perfect for the proper aspirer with a vintage kind of taste. ", image_url: "wheel-3184641_1920.jpg", colour: "black", price: "2499.95"}
 ])
