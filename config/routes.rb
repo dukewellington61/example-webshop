6c7a3b88b2563036f8214ad2f73ab4558ec276c0
dukewellington61 <dukewellington61@googlemail.com>
2019-02-04 10:25:55 +0100

    5.5 authentation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   post 'simple_pages/thank_you'
   get 'simple_pages/landing_page'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#landing_page'  
   resources :orders, only: [:index, :show, :create, :destroy]
