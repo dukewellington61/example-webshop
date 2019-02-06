48d2022600d54a195a009e5d9a7c5bf01dd0c35a
dukewellington61 <dukewellington61@googlemail.com>
2019-02-07 00:01:10 +0100

    5.8 raty following the instructions in the course material have messed up my app

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
-  resources :users
-  resources :products
+
+  resources :products do
+  resources :comments
+end
+
+resources :users
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   post 'simple_pages/thank_you'
   get 'simple_pages/landing_page'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'  
+  root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
+
+
+
+
 end
