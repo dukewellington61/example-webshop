3f4b78bcf789413ef354ca3bce727a8ae27eb115
dukewellington61 <dukewellington61@googlemail.com>
2019-02-10 16:29:17 +0100

    5.10 seed3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {first_name: nil, last_name: nil, email: "admin@test.de", encrypted_password: "$2a$11$c7tXdHzRTP3p0YBXACCv4eFA5WOvTW1fxExkntNLUoYj2QIfDnRjK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: false}
+  {first_name: nil, last_name: nil, email: "admin@test.de", password: "some_password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: false}
 ])
