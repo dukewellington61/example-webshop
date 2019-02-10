9b67859b3c9ddaa3d6b13c3da9a600643c692079
dukewellington61 <dukewellington61@googlemail.com>
2019-02-10 16:37:46 +0100

    5.10 seed3

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :read, :all
+    can :manage, :all
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {first_name: nil, last_name: nil, email: "admin@test.de", password: "some_password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: false}
+  {first_name: nil, last_name: nil, email: "andreas@admin.de", password: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: true}
 ])
