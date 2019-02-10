80e5db2142adabf05d4da061d1341b49c3813b56
dukewellington61 <dukewellington61@googlemail.com>
2019-02-10 21:44:38 +0100

    5.10 more changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, :all
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
     end
   end
+end
 
     # Define abilities for the passed in user here. For example:
     #
