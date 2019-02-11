39d87bc5a3a535eb08a47a4f1ed65902f48d6127
dukewellington61 <dukewellington61@googlemail.com>
2019-02-12 00:02:41 +0100

    3.11.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,20 +92,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "duke-wellington-example-app.herokuapp.com" }
 
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'https://duke-wellington-example-app.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+  :from                 => 'andreas_koeth@gmx.de',
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
+  :domain               => 'https://duke-wellington-example-app.herokuapp.com',
+  :address              => 'smtp.sendgrid.net',
+  :port                 => 587,
+  :authentication       => :plain,
+  :enable_starttls_auto => true  
 
 
 end
