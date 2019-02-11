803ade5939614ae445b5db0f7b3818222588bbb2
dukewellington61 <dukewellington61@googlemail.com>
2019-02-11 23:38:12 +0100

    5.11 code to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "duke-wellington-example-app.herokuapp.com" }
 
-end
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
+
+end
