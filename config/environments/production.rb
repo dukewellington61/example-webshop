2124854034b563040813de078d6618e5e2da78e8
dukewellington61 <dukewellington61@googlemail.com>
2019-02-12 12:50:49 +0100

    5.11.6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,6 @@ Rails.application.configure do
 
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
@@ -104,7 +103,7 @@ Rails.application.configure do
   :from                 => 'andreas_koeth@gmx.de',
   :user_name            => ENV['SENDGRID_USERNAME'],
   :password             => ENV['SENDGRID_PASSWORD'],
-  :domain               => 'heroku.com',
+  :domain               => 'duke-wellington-example-app.herokuapp.com',
   :address              => 'smtp.sendgrid.net',
   :port                 => 587,
   :authentication       => :plain,
