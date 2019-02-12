638fb037dbf182b2b3d0a492c94bd6a7db4a3de1
dukewellington61 <dukewellington61@googlemail.com>
2019-02-12 12:15:39 +0100

    5.11.5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: "duke-wellington-example-app.herokuapp.com" }
 
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'https://duke-wellington-example-app.herokuapp.com' }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
