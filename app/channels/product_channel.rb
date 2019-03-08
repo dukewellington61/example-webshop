33a3d22f3b95f54d0151ad095007d8b85c840b8c
dukewellington61 <dukewellington61@googlemail.com>
2019-03-08 16:27:59 +0100

    6.14.1 ActiveJob and Advanced ActionCable

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -6,4 +6,9 @@ class ProductChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def listen data
+  	stop_all_streams
+  	stream_for data["product_id"]
+  end
 end
