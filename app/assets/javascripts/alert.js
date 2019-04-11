9cda0ef4a316e494048f8ef9395fe1b5e9982ddb
dukewellington61 <dukewellington61@googlemail.com>
2019-04-11 21:48:26 +0200

    24. shopping cart

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -7,10 +7,3 @@ $(document).on('turbolinks:load', function(){
 	$("#notice").delay(500).fadeOut(50000);
 
 });
-
-$(document).ready(function () {
-        if (window.location.href == 'http://localhost:3000/payments/create') {
-          $("#line_items_count").hide();
-          $(".navbar_cart").append('0');
-        }
-    });
--- app/assets/javascripts/products.coffee
+++ app/assets/javascripts/products.coffee
@@ -3,5 +3,4 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-   $('#line_items_count').click ->
   
