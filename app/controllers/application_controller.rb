6c6554034287db73850f2d8f9bdfdabcb738a6bb
dukewellington61 <dukewellington61@googlemail.com>
2019-04-12 17:26:02 +0200

    25. shopping_cart redirect previous page after sign in log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   include CurrentCart
+
   before_action :set_cart
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
 
+
+    def after_sign_in_path_for(resource_or_scope)
+        stored_location_for(resource_or_scope) || super
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        request.referer
+    end
 
   protected
 
@@ -15,8 +26,19 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_url, alert: exception.message
     end
 
-    def after_sign_in_path_for(resource)
-    users_path #your path
-    end
+  private
+   # Its important that the location is NOT stored if:
+   # - The request method is not GET (non idempotent)
+   # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+   #    infinite redirect loop.
+   # - The request is an Ajax request as this can lead to very unexpected behaviour.
+   def storable_location?
+     request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+   end
+
+   def store_user_location!
+     # :user is the scope we are authenticating
+     store_location_for(:user, request.fullpath)
+   end
 
 end
